6300ef35c41c46250369b8e76ea172a934dc8d9b
dud3 <dk18184@gmail.com>
2015-08-29 22:51:27 +0200

    feature: :gem: ability to remove objects and refactor as well.

--- js/olm.js
+++ js/olm.js
@@ -179,6 +179,7 @@ var ToolsController = (function () {
         this.$scope.$ = this;
         document.getElementById('fileupload').addEventListener('change', this.onFileSelect, false);
         msg.on('confirmation-clicked', this.onSaveProjectConfirm, this);
+        keys.bindKey(46, this.removeObject, this);
         keys.bindCtrl(73, this.addSymobol, this);
         keys.bindCtrl(84, this.addText, this);
         keys.bindCtrl(85, this.uploadImage, this);
@@ -190,6 +191,9 @@ var ToolsController = (function () {
     ToolsController.prototype.onTutorialClick = function () {
         open("http://www.onlinelogomaker.com/tutorial", "_blank");
     };
+    ToolsController.prototype.removeObject = function () {
+        msg.send('remove-object');
+    }
     ToolsController.prototype.addSymobol = function () {
         msg.send('open-symbols');
     };
@@ -291,8 +295,8 @@ var SymbolsController = (function () {
         msg.on('open-symbols', this.open, this);
         this.$scope.$ = this;
         this.htmlString = htmlString(svgImg);
-    
-        // NOTE: It was commented: 
+
+        // NOTE: It was commented:
         // starting from -->
         /*
         this.$http.get('assets/symbols/animals.json').then(function (res) {
@@ -309,7 +313,7 @@ var SymbolsController = (function () {
         });
         */
         // ending at <--
-        
+
         $scope.$.categories[0].content = [1, 2, 3];
     }
     SymbolsController.prototype.open = function () {
@@ -331,8 +335,6 @@ var SymbolsController = (function () {
         this.categories[this.selectedCategory].cssClass = "category-item-selected";
     };
     SymbolsController.prototype.onSymbolClick = function (index, $event) {
-        console.log($event.currentTarget);
-        console.log($event.currentTarget.children);
         $('.selected').removeClass('selected');
         if($event.currentTarget.children.length > 0) {
             for(var key in $event.currentTarget.children) {
@@ -387,9 +389,9 @@ var CanvasController = (function () {
         canvas.setHeight(600);
 
         var svg = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n  <defs/>\n  <g>\n    <path stroke=\"none\" fill=\"#6633ff\" d=\"M31.4 0.35 Q34.45 1.25 36.6 4.35 37.9 6.3 38.5 8.65 39.1 10.85 38.95 13.25 38.85 14.9 38.35 16.4 L38.15 17.05 Q37.55 18.8 36.55 20.2 L38.1 21.4 39.4 22.9 Q40.45 24.4 40.45 25.8 40.5 27.15 39.65 28.15 38.95 29 37.75 29.35 L35.1 29.65 32.65 29.2 Q31.5 28.8 30.45 28.2 28.5 27 27.35 25.4 L26.85 24.55 26.55 23.75 Q25.1 23.2 23.85 22.2 22.65 21.25 21.75 19.9 20.65 18.3 20.05 16.4 L19.6 14.75 Q19.3 13.1 19.4 11.4 19.45 9.5 20 7.8 20.7 5.4 22.15 3.55 23.65 1.7 25.7 0.8 28.5 -0.5 31.4 0.35 M35.1 6.7 Q33.3 4.1 30.75 3.2 28.35 2.35 25.95 3.2 24.15 3.8 22.75 5.3 21.4 6.75 20.65 8.8 20.15 10.3 20 11.95 19.9 13.5 20.15 15.05 L20.5 16.4 Q21 18.25 22.1 19.85 L24.1 22 Q25.25 22.9 26.6 23.35 L26.95 24.15 27.4 24.9 Q28.45 26.45 30.2 27.45 L32.2 28.3 34.4 28.65 36.7 28.25 Q37.7 27.85 38.3 27.05 38.95 26.15 38.85 24.95 38.8 23.65 37.9 22.35 L36.75 21.05 Q36.15 20.5 35.4 20.05 36.25 18.8 36.7 17.3 L36.95 16.4 37.2 14.05 Q37.3 12.1 36.75 10.25 36.25 8.35 35.1 6.7 M8.95 18.6 L11.4 18.3 14.75 18.55 Q17.65 19.1 19.9 20.65 21.3 21.6 22.2 22.8 23.25 24.05 23.75 25.45 L24.55 25.8 25.4 26.3 Q27.05 27.45 28.2 29.35 L29.2 31.55 29.7 34 Q29.8 35.5 29.4 36.7 29 37.9 28.15 38.6 27.15 39.45 25.8 39.4 24.4 39.35 22.9 38.3 22.1 37.8 21.4 37.05 L20.25 35.45 Q18.8 36.45 17.05 37.1 15.25 37.75 13.25 37.85 L8.95 37.5 8.65 37.45 Q6.3 36.85 4.4 35.5 1.25 33.35 0.35 30.3 -0.5 27.45 0.8 24.6 1.7 22.6 3.6 21.1 5.4 19.65 7.8 18.9 L8.95 18.6 M5.3 21.65 Q3.8 23.05 3.2 24.85 2.35 27.3 3.2 29.7 4.1 32.25 6.75 34.05 7.75 34.75 8.95 35.25 L10.25 35.7 14.05 36.15 17.3 35.6 Q18.8 35.15 20.05 34.35 L21.1 35.7 22.35 36.8 Q23.65 37.7 24.95 37.8 26.15 37.85 27.05 37.2 27.85 36.65 28.25 35.6 28.7 34.6 28.65 33.3 28.65 32.25 28.3 31.15 L27.45 29.15 Q26.4 27.35 24.9 26.3 L24.15 25.85 23.4 25.55 Q22.9 24.2 22 23 21.1 21.9 19.85 21 17.7 19.55 15.05 19.1 L12 18.95 8.95 19.55 8.8 19.6 Q6.75 20.35 5.3 21.65\"/>\n  </g>\n</svg>";
-        
+
         // console.log(svg.toString());
-        msg.send('add-symbol', svg);
+        // msg.send('add-symbol', svg);
 
         /*
             fabric.loadSVGFromString(svg.toString(), function (objects, options) {
@@ -399,7 +401,7 @@ var CanvasController = (function () {
                  canvas.setActiveObject(obj);
                  // console.log("loaded");
              });
-        
+
              var circle = new fabric.Circle({
                  radius: 100,
                  fill: '#eef',
@@ -409,12 +411,14 @@ var CanvasController = (function () {
              });
              canvas.add(circle);
          */
-         
+
         canvas.on('object:moving', this.onObjectMove);
         canvas.on('object:selected', this.onObjectSelect);
         msg.on('add-symbol', this.addSymbol, this);
         msg.on('add-image', this.addImage, this);
         msg.on('add-text', this.addText, this);
+        msg.on('remove-object', this.removeObject, this);
+        msg.on('remove-objects', this.removeObjects, this);
         msg.on('save-canvas', this.saveCanvas, this);
         msg.on('update-undo-redo-state', this.updateUndoRedoState, this);
         $cmd.map('move', this.move, this);
@@ -427,11 +431,11 @@ var CanvasController = (function () {
         $cmd.map('add-object', function (obj) {
             canvas.add(obj).renderAll();
             canvas.setActiveObject(obj);
-  
+
             msg.send('object-added', obj);
           }, this);
         // $cmd.mapUndo('add-object', canvas.remove);
-        
+
         $cmd.map('text-color', this.textColor, this, 0 /* SWITCH */);
         $cmd.map('text-font', this.textFont, this);
         $scope.undoActive = false;
@@ -462,7 +466,7 @@ var CanvasController = (function () {
         this.selected.set({
             scaleY: y,
             scaleX: x,
-            originX: "center", 
+            originX: "center",
             originY: "center"
         });
         msg.send('update-scale');
@@ -530,12 +534,27 @@ var CanvasController = (function () {
         }
     };
     CanvasController.prototype.removeGrid = function() {
-        canvas.getObjects('line').forEach(function(obj) { 
-            if(typeof obj != 'undefined') { 
-                console.log(obj); 
-                obj.remove(); 
-            } 
+        canvas.getObjects('line').forEach(function(obj) {
+            if(typeof obj != 'undefined') {
+                console.log(obj);
+                obj.remove();
+            }
         });
+    };
+    CanvasController.prototype.removeObject = function() {
+        if(canvas.getActiveGroup()) {
+          canvas.getActiveGroup().forEachObject(function(o){ $cmd.run('remove', o); });
+          canvas.discardActiveGroup().renderAll();
+        } else {
+          $cmd.run('remove', canvas.getActiveObject());
+        }
+    };
+    CanvasController.prototype.removeObjects = function() {
+        if(canvas.getObjects() != null) {
+            canvas.forEachObject(function(o){ $cmd.run('remove', o); });
+            // $cmd.run('remove-objects', canvas.getObjects());
+            // canvas.clear().renderAll();
+        }
     };
     CanvasController.prototype.saveCanvas = function () {
 
@@ -563,7 +582,7 @@ var CanvasController = (function () {
         // Get rid of the week day.
         split_date.splice(0, 1);
         var month = split_date[0],
-            day = split_date[1], 
+            day = split_date[1],
             year = split_date[2].slice(2, 4); // Get the last two digits.
 
         var time = split_date[3].split(':');
@@ -687,7 +706,6 @@ var TransformController = (function () {
         var _this = TransformController.instance;
         _this.$scope.width = e.target.get('width') * e.target.scaleX;
         _this.$scope.height = e.target.get('height') * e.target.scaleY;
-        ;
         _this.$scope.$apply();
         console.log("scale", e.target.get('width'));
     };
@@ -875,13 +893,13 @@ var ModifyController = (function () {
     ModifyController.prototype.zoomIn = function () {
 
         /*  // Just testing scaling...
-            setInterval(function() { 
-                for(var key in objs) { 
-                    con += 0.1; 
-                    console.log(objs[key]); 
-                    objs[key].setScaleX(con); 
-                    objs[key].setScaleY(con); 
-                } 
+            setInterval(function() {
+                for(var key in objs) {
+                    con += 0.1;
+                    console.log(objs[key]);
+                    objs[key].setScaleX(con);
+                    objs[key].setScaleY(con);
+                }
             }, 3000);
         */
 
@@ -894,7 +912,7 @@ var ModifyController = (function () {
         /*
             if(canvas.getActiveGroup() != null) {
                 for(var key in canvas.getActiveGroup().getObjects()) {
-                    objs[key].setScaleX(1.4) 
+                    objs[key].setScaleX(1.4)
                 }
             }
         */
@@ -983,15 +1001,6 @@ var LoginController = (function () {
 
         $(".auth-content").html('');
 
-        if(typeof this.$scope.username != 'undefined') {
-            if(this.$scope.username.length == 0) {
-                _auth_flag = true;
-                _auth_msg = "Please enter your username";
-            }
-        } else {
-            _auth_flag = true;
-            _auth_msg = "Please enter your username";
-        }
         if(typeof this.$scope.password != 'undefined') {
             if(this.$scope.password.length == 0) {
                 _auth_flag = true;
@@ -1000,6 +1009,15 @@ var LoginController = (function () {
         } else {
             _auth_flag = true;
             _auth_msg = "Please enter your password";
+        }
+        if(typeof this.$scope.username != 'undefined') {
+            if(this.$scope.username.length == 0) {
+                _auth_flag = true;
+                _auth_msg = "Please enter your username";
+            }
+        } else {
+            _auth_flag = true;
+            _auth_msg = "Please enter your username";
         }
 
         if(_auth_flag) {
@@ -1085,12 +1103,10 @@ var LoginController = (function () {
     };
     LoginController.prototype.onOkClick = function () {
         this.$scope.visible = false;
-        $("#id-auth").hide();
         msg.send("close-popups");
     };
     LoginController.prototype.onCancelClick = function () {
         this.$scope.visible = false;
-        ("#id-auth").hide();
         msg.send("close-popups");
     };
     return LoginController;
@@ -1104,6 +1120,9 @@ var ObjectsController = (function () {
         $scope.$ = this;
         msg.on('object-added', this.onObjectAdd, this);
         msg.on('object-selected', this.onObjectSelected, this);
+        msg.on('object-single-delete', this.onDeleteClick, this);
+        msg.on('object-multiple-delete', this.onDeleteAllClick, this);
+
         // canvas.on('object:selected', this.onObjectSelected);
         // canvas.on('selection:cleared', this.onSelectionCleared);
         $scope.items = $canvas.children;
@@ -1132,6 +1151,7 @@ var ObjectsController = (function () {
         var disObj = new DisplayObject();
         disObj.raw = obj;
         this.items.push(disObj);
+        alert("onObjectAdd");
         // disObj.type = obj.type.toUpperCase();
         // if (disObj.type == "PATH-GROUP") disObj.type = "SYMBOL";
         disObj.name = "OBJECT " + this.items.length;
@@ -1165,9 +1185,17 @@ var ObjectsController = (function () {
     ObjectsController.prototype.onRenameClick = function () {
         msg.send('open-popup', 'rename');
     };
+    ObjectsController.prototype.removeObj = function () {
+        msg.send('remove-object');
+    }
     ObjectsController.prototype.onDeleteClick = function () {
+        this.removeObj();
     };
+    ObjectsController.prototype.removeAllObj = function () {
+        msg.send('remove-objects');
+    }
     ObjectsController.prototype.onDeleteAllClick = function () {
+        this.removeAllObj();
     };
     return ObjectsController;
 })();
@@ -1347,6 +1375,7 @@ var SymbolColorController = (function () {
         $scope.$ = this;
         $scope.visible = false;
         $scope.pathsVisible = true;
+
         msg.on('object-selected', this.onObjectSelected, this);
         msg.on('paths-selected', this.onObjectSelected, this);
         $(".color-picker")["mlColorPicker"]({
@@ -1438,7 +1467,9 @@ var KeysBinder = (function () {
         }
         else if (!this.ctrlPressed && cbkey) {
             cbkey.call(this.keyBindsCallers[evtobj.keyCode]);
-            this.ctrlBindsCallers[evtobj.keyCode].$scope.$apply();
+            if(typeof this.ctrlBindsCallers[evtobj.keyCode] != 'undefined') {
+                this.ctrlBindsCallers[evtobj.keyCode].$scope.$apply();
+            }
         }
     };
     KeysBinder.prototype.onKeyUp = function (e) {
@@ -1545,6 +1576,8 @@ var CanvasService = (function () {
         });
         $cmd.map('add', this.add, this);
         $cmd.mapUndo('add', this.remove);
+        $cmd.map('remove', this.remove, this);
+        $cmd.mapUndo('remove', this.add, this);
         $cmd.map('shadow', this.shadow, this, 0 /* SWITCH */);
         $cmd.map('symbol-color', this.symbolColor, this, 0 /* SWITCH */);
         this.root.on('object:selected', function (e) {
@@ -1556,7 +1589,7 @@ var CanvasService = (function () {
         var width = this.root.getWidth();
         var height = this.root.getHeight();
         var pixels = this.root.getContext().getImageData(0, 0, width, height);
-        //var mouseDownHandler 
+        //var mouseDownHandler
         this.root.on('mouse:down', this.onMouseDown);
         window.requestAnimationFrame(this.renderLoop);
     }
@@ -1575,9 +1608,11 @@ var CanvasService = (function () {
         return dobj;
     };
     CanvasService.prototype.add = function (dobj) {
+        console.log(dobj);
         dobj.selected = true;
         this.root.add(dobj.raw).renderAll();
         this.children.push(dobj);
+        console.log(this.children);
         this.select(dobj);
         if (dobj.type == DOType.SYMBOL) {
             for (var i = 0; i < dobj.raw.getObjects().length; i++) {
@@ -1600,7 +1635,7 @@ var CanvasService = (function () {
             canvas.discardActiveObject();
             this.unselectAll();
         }
-        this.root.remove(dobj.raw);
+        this.root.remove(dobj);
         console.log('remove', dobj, dobj.raw);
         this.root.renderAll();
     };
