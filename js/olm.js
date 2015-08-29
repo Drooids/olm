fc02b4818f2ecee81ddfb77efed6fc2753747c55
dud3 <dk18184@gmail.com>
2015-08-29 14:07:27 +0200

    fix: :crystal_ball: layout ToolsController

--- js/olm.js
+++ js/olm.js
@@ -1732,7 +1732,7 @@ var NgController = (function () {
     NgController.ext = [];
     return NgController;
 })();
-/*	This work is licensed under Creative Commons GNU LGPL License.
+/*  This work is licensed under Creative Commons GNU LGPL License.
 
     License: http://creativecommons.org/licenses/LGPL/2.1/
    Version: 0.9
